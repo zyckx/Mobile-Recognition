0c29f8fbabd327e4eed6bb827199f908952d81d2
galaxyZhang <694553456@qq.com>
2023-08-30 23:18:31 +0800

    feat: 修改主页样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,47 +1,10 @@
 // index.js
 // 获取应用实例
 const app = getApp();
-const imageCdn = 'https://tdesign.gtimg.com/miniprogram/images';
-const swiperList = [
-	{
-		value: `${imageCdn}/swiper1.png`,
-		ariaLabel: '图片1',
-	},
-	{
-		value: `${imageCdn}/swiper2.png`,
-		ariaLabel: '图片2',
-	},
-	{
-		value: `${imageCdn}/swiper1.png`,
-		ariaLabel: '图片1',
-	},
-	{
-		value: `${imageCdn}/swiper2.png`,
-		ariaLabel: '图片2',
-	},
-];
+import ActionSheet, { ActionSheetTheme } from 'tdesign-miniprogram/action-sheet/index';
 Page({
 	data: {
-		current: 1,
-		autoplay: true,
-		duration: 500,
-		interval: 5000,
-		swiperList,
-		img1: 'https://tdesign.gtimg.com/miniprogram/images/example1.png',
-		img2: 'https://tdesign.gtimg.com/miniprogram/images/example2.png',
-		img3: 'https://tdesign.gtimg.com/miniprogram/images/example3.png',
-		visible: true,
-		marquee1: {
-			speed: 80,
-			loop: -1,
-			delay: 0,
-		},
-		marquee2: {
-			speed: 60,
-			loop: -1,
-			delay: 0,
-		},
-		content: ['君不见', '高堂明镜悲白发', '朝如青丝暮成雪', '人生得意须尽欢', '莫使金樽空对月'],
+	
 	},
 	onShow() {
 		if (typeof this.getTabBar === "function" && this.getTabBar()) {
@@ -52,4 +15,72 @@ Page({
 		}
 	},
 	onLoad() { },
+	handleAction() {
+		ActionSheet.show({
+		  theme: ActionSheetTheme.List,
+		  selector: '#t-action-sheet',
+		  context: this,
+		  items: [
+			{
+			  label: '选项一',
+			},
+			{
+			  label: '选项二',
+			},
+			{
+			  label: '选项三',
+			},
+			{
+			  label: '选项四',
+			},
+		  ],
+		});
+	  },
+	  showDescAction() {
+		ActionSheet.show({
+		  theme: ActionSheetTheme.List,
+		  selector: '#t-action-sheet',
+		  context: this,
+		  description: '动作面板描述文字',
+		  items: [
+			{
+			  label: '选项一',
+			},
+			{
+			  label: '选项二',
+			},
+			{
+			  label: '选项三',
+			},
+		  ],
+		});
+	  },
+	  showIconAction() {
+		ActionSheet.show({
+		  theme: ActionSheetTheme.List,
+		  selector: '#t-action-sheet',
+		  context: this,
+		  items: [
+			{
+			  label: '选项一',
+			  icon: 'app',
+			},
+			{
+			  label: '选项二',
+			  icon: 'app',
+			},
+			{
+			  label: '选项三',
+			  icon: 'app',
+			},
+			{
+			  label: '选项四',
+			  icon: 'app',
+			},
+		  ],
+		});
+	  },
+	  handleSelected(e) {
+		console.log(e.detail);
+	  }
 });
